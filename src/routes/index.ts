23969c6086f3e6a26307cdeb524158b84fe9eed4
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-11-06 12:11:04 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Request, Router, Response } from 'express'
+
+import * as PageController from '../controlers/pageController'
+import * as SearchController from '../controlers/searchController'
+
+const router = Router()
+
+router.get('/', PageController.home)
+router.get('/dogs', PageController.dogs)
+router.get('/cats', PageController.cats)
+router.get('/fishes', PageController.fishes)
+
+router.get('/search', SearchController.search)
+
+
+export default router
